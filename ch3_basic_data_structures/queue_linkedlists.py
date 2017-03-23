17592cae073e9c923b28795a9e231da1ac9f9732
Allison <allisonyee56@gmail.com>
2017-03-23 12:02:22 -0400

    added files

---
+++ ch3_basic_data_structures/queue_linkedlists.py
@@ -0,0 +1,50 @@
+
+from node import Node
+
+
+class Queue:
+
+    def __init__(self):
+        self.front = None
+        self.back = None
+        self.N = 0
+
+    def enqueue(self, data):
+        temp = Node(data)
+        if self.front is None:
+            self.front = temp
+            self.back = self.front
+        else:
+            self.back.setNext(temp)
+            self.back = self.back.getNext()
+        self.N += 1
+
+    def dequeue(self):
+        if self.isEmpty():
+            print ("Queue is EMPTY!")
+            return None
+        temp = self.front
+        if self.front.getNext() is None:
+            self.front = None
+            self.back = None
+        else:
+            self.front = self.front.getNext()
+        self.N -= 1
+        return temp
+
+    def qprint(self):
+        if not self.isEmpty():
+            node = self.front
+            while not node is None:
+                print (node.data, end=" -> ")
+                node = node.getNext()
+            print("null")
+        else:
+            print ("Queue is EMPTY!")
+
+
+    def isEmpty(self):
+        return self.N == 0
+
+    def size(self):
+        return self.N
