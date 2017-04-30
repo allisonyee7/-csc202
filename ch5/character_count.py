901f991a9bb941350948828aa5ce9c986d1edc46
Allison <allisonyee56@gmail.com>
2017-04-30 19:55:40 -0400

    modified

---
+++ ch5/character_count.py
@@ -0,0 +1,30 @@
+from string import ascii_uppercase, digits
+
+    """
+    Return a list containing counts of the number of occurances of each
+    ASCII character in s.
+      >>> result1 = char_counts("abbcccddddeeeeeffffff")
+      >>> len(result1)
+      256
+      >>> result1[96:105]
+      [0, 1, 2, 3, 4, 5, 6, 0, 0]
+      >>> result2 = char_counts("AACEEEGIIJLLL")
+      >>> result2[64:78]
+      [0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 1, 0, 3, 0]
+      >>> result3 = char_counts("this is seriously silly")
+      >>> result3[ord('t')]
+      1
+      >>> result3[ord('i')]
+      4
+      >>> result3[ord('2')]
+      5
+    """
+    counts = [0] * 256
+
+    for ch in s:
+        counts[ord(ch)] += 1
+    return counts
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
--- ch5/hash_table.py
+++ ch5/hash_table.py
@@ -3,3 +3,52 @@ class HashTable:
         self.size = 11
         self.slots = [None] * self.size
         self.data = [None] * self.size
+
+    def __setitem__(self, key, data):
+        self.put(key, data)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def hash_function(self, key, size):
+        return hash(key, size)
+
+    def rehash(self, old_hash, size):
+        return (old_hash + 1) % size
+
+    def get(self, key):
+        start_slot = self.hash_function(key, len(self.slots))
+
+        data = None
+        index = start_slot
+        while self.slots[index] is not None:
+            if self.slots[index] == key:
+                data = self.data[index]
+                break
+            else:
+                index = self.rehash(index, len(self.slots))
+                if index == start_slot:
+                    break
+        return data
+
+    def put(self, key, data):
+        hashVal = self.hash_function(key, len(self.slots))
+
+        if self.slots[hashVal] is None:
+            self.slots[hashVal] = key
+            self.data[hashVal] = data
+        elif self.slots[hashVal] == key:
+            self.data[hashVal] = data   # replace
+        else:
+            next_slot = self.rehash(hashVal, len(self.slots))
+            while self.slots[next_slot] is not None and \
+                    self.slots[next_slot] != key:
+                next_slot = self.rehash(next_slot, len(self.slots))
+
+            if self.slots[next_slot] is None:
+                self.slots[next_slot] = key
+                self.data[next_slot] = data
+            else:
+		self.data[next_slot] = data
+
+
